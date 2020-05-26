87edd520fc49673851a2031fde3f18a59661aaf6
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2020-05-25 22:00:41 -0300

    criando sessão de notícias

---
+++ pages/noticias.php
@@ -0,0 +1,53 @@
+<section class="header-noticias">
+	<div class="center">
+		<h2><i class="fa fa-bell-o" aria-hidden="true"></i></h2>
+		<h2>Acompanhe as últimas <b>notícias do portal</b></h2>
+	</div>
+	<!-- center -->
+</section>
+<!-- header-noticias -->
+<section class="container-portal">
+	<div class="center">
+		<div class="sidebar">
+			<div class="box-content-sidebar">
+				<h3><i class="fa fa-search" aria-hidden="true"></i> Realizar uma busca</h3>
+				<form>
+					<input type="text" name="busca" placeholder="O que deseja procurar?" required />
+					<input type="submit" name="acao" value="Pesquisar!" />
+				</form>
+			</div>
+			<!-- box-content-sidebar -->
+			<div class="box-content-sidebar">
+				<h3><i class="fa fa-list-ul" aria-hidden="true"></i> Selecione a categoria</h3>
+				<form>
+					<select name="categoria">
+						<option value="esportes">Esportes</option>
+						<option value="geral">Geral</option>
+					</select>
+				</form>
+			</div>
+			<!-- box-content-sidebar -->
+			<div class="box-content-sidebar">
+				<h3><i class="fa fa-user" aria-hidden="true"></i> Sobre o autor</h3>
+				<div class="autor-box-portal">
+					<div class="box-img-autor"></div>
+					<!-- box-img-autor -->
+					<div class="texto-autor-portal text-center">
+						<h3>Gustavo Alves</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamc</p>
+					</div>
+					<!-- texto-autor-portal -->
+				</div>
+				<!-- autor-box-portal -->
+			</div>
+			<!-- box-content-sidebar -->
+		</div>
+		<!-- sidebar -->
+		<div class="clear"></div>
+		<!-- clear -->
+	</div>
+	<!-- center -->
+</section>
+<!-- container-portal -->
